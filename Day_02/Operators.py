e4e0dfce3a2727ac25d3ca5151dd76ffdcafc72c
Minato-07 <mdmusfiq.hasan@gmail.com>
2026-01-19 03:49:33 +0600

    Day two

---
+++ Day_02/Operators.py
@@ -0,0 +1,117 @@
+# Variables
+a = 15
+b = 4
+
+#Arithmetic Operators
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
+
+# Floor Division
+print("Floor Division:", a // b)
+
+# Modulus
+print("Modulus:", a % b)
+
+# Exponentiation
+print("Exponentiation:", a ** b)
+
+
+#----------------###----------------#
+print()
+#Comparison Operators
+a = 13
+b = 33
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+#----------------###----------------#
+print()
+#Logical Operators
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+
+#----------------###----------------#
+print()
+#Bitwise Operators
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >> 2)
+print(a << 2)
+
+#----------------###----------------#
+print()
+
+#Assignment Operators
+a = 10
+b = a
+print(b)
+b += a
+print(b)
+b -= a
+print(b)
+b *= a
+print(b)
+b <<= a
+print(b)
+
+#----------------###----------------#
+print()
+#Identity Operators
+a = 10
+b = 20
+c = a
+
+print(a is not b)
+print(a is c)
+
+#----------------###----------------#
+print()
+
+#Membership Operators
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if (x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
+
+#----------------###----------------#
+print()
+
+#Ternary Operator
+a, b = 10, 20
+min = a if a < b else b
+
+print(min)
+#----------------###----------------#
+print()
---
+++ Day_02/Variables.py
@@ -0,0 +1,66 @@
+x = 5
+print(x)
+y = 3.14
+z = "hi"
+x = "Now a string"
+
+#----------------###----------------#
+
+
+#Assigning multiple (same) values
+a = b = c = 154
+print(a, b, c)
+
+#----------------###----------------#
+
+
+#Assigning multiple (different) values
+m , n , o = 1, 2.5, "python"
+print(m, n, o)
+
+
+#----------------###----------------#
+
+
+#Data typecasting
+s = "10"
+print(int(s))
+print(float(s))
+print(str(s))
+
+#----------------###----------------#
+
+
+#Showing data types using type() function
+n = 42
+f = 3.24
+s = "hello world"
+li = [1, 2, 3]
+d = {'key':1, 'value':2}
+bool = True
+print(type(n))
+print(type(f))
+print(type(s))
+print(type(li))
+print(type(d))
+print(type(bool))
+
+
+#----------------###----------------#
+
+
+#Swapping two variables using multiple assignments
+a, b = 4, 16
+print(a, b)
+a, b = b, a
+print(a, b)
+
+
+#----------------###----------------#
+
+
+#length of string
+word = "what are you doing!"
+print(len(word))
+
+#----------------###----------------#
